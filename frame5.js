b3b0ad089b2b7e63a7d4e2bc583083ee0a338a27
notsaiki <78707529+notsaiki@users.noreply.github.com>
2024-12-09 12:39:50 +0100

    first

---
+++ frame5.js
@@ -0,0 +1,8 @@
+document.getElementById('account-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const thankYouMessage = document.getElementById('thank-you-message');
+    thankYouMessage.classList.add('show');
+  
+    document.getElementById('account-form').reset();
+  });
+  
