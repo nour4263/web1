221ecb6e482b2f305f6390a644b93f4d3e6c6393
yasmine <zoghlamiyasmine@gmail.com>
2024-12-10 00:59:57 +0100

    ajout des fichiers

---
+++ frame7.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.stars span').forEach(star => {
+    star.addEventListener('click', function () {
+      const rating = this.getAttribute('data-value');
+      alert(`You rated us ${rating} stars!`);
+    });
+  });
+  
+  document.querySelector(".back-button").addEventListener("click", function () {
+    const previousURL = this.getAttribute("data-url");
+    if (previousURL) {
+      window.location.href = previousURL;
+    } else {
+      console.error("No URL specified for the back button.");
+    }
+  });
+  
