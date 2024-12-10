7f211f6cb5c349b5766f0bc2060cf62dcfd9b35f
notsaiki <78707529+notsaiki@users.noreply.github.com>
2024-12-10 22:22:32 +0100

    frame7.js

--- frame7.js
+++ frame7.js
@@ -13,10 +13,3 @@ document.querySelectorAll('.stars span').forEach(star => {
       console.error("No URL specified for the back button.");
     }
   });
-=======
-document.querySelectorAll('.stars span').forEach(star => {
-    star.addEventListener('click', function () {
-      const rating = this.getAttribute('data-value');
-      alert(`You rated us ${rating} stars!`);
-    });
-  }); 
