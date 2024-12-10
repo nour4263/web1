fdd75901f02cbf5daccd0df8245e6ef03d00b486
notsaiki <78707529+notsaiki@users.noreply.github.com>
2024-12-10 22:23:53 +0100

    frame4.js

--- frame4.js
+++ frame4.js
@@ -2,8 +2,3 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault(); 
     alert('Form submitted!');
   });
-=======
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    alert('Form submitted!');
-  });
